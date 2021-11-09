d86d0750d6cdea8eb1e329a446d7ee3757228241
unknown <luis8e@hotmail.com>
2021-11-09 13:22:30 -0500

    correction in the example files

--- src/Tests/EmployeeOverlap.java
+++ src/Tests/EmployeeOverlap.java
@@ -14,33 +14,33 @@ class EmployeeOverlap {
 
 	@Test
 	void equalsListExample1() {
-		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Main\\Schedule");
+		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Main\\Schedule.txt");
 		assertEquals(3,employeesList.size());
 	}
 	
 	@Test
 	void equalsListExample2() {
-		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Tests\\Example2");
+		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Tests\\Example2.txt");
 		assertEquals(2,employeesList.size());
 	}
 	
 	@Test
 	void equalsTableExample1() {
-		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Main\\Schedule");
+		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Main\\Schedule.txt");
 		Hashtable<String, Integer> result = EmployeeFile.employeeOverlap(employeesList);
 		assertEquals(3,result.keySet().size());
 	}
 	
 	@Test
 	void equalsTableExample2() {
-		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Tests\\Example2");
+		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Tests\\Example2.txt");
 		Hashtable<String, Integer> result = EmployeeFile.employeeOverlap(employeesList);
 		assertEquals(1,result.keySet().size());
 	}
 	
 	@Test
 	void equalsTableContentExample1() {
-		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Main\\Schedule");
+		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Main\\Schedule.txt");
 		Hashtable<String, Integer> result = EmployeeFile.employeeOverlap(employeesList);
 		assertEquals(2,result.get("RENE-ASTRID"));
 		assertEquals(3,result.get("ASTRID-ANDRES"));
@@ -49,7 +49,7 @@ class EmployeeOverlap {
 	
 	@Test
 	void equalsTableContentExample2() {
-		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Tests\\Example2");
+		LinkedList<Employee> employeesList = EmployeeFile.readEmployeeFile("src\\Tests\\Example2.txt");
 		Hashtable<String, Integer> result = EmployeeFile.employeeOverlap(employeesList);
 		assertEquals(3,result.get("RENE-ASTRID"));
 	}
